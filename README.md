437bd10f6e6caaef2363585cb8f3956b2dd7a28d
gitnassima20 <nassimabaroudi8@gmail.com>
2023-11-14 15:07:48 +0100

    adding fastpi

--- README.md
+++ README.md
@@ -4,13 +4,5 @@
 
 ## Overview
 
-This project is an example of building microservices using FastAPI and integrating continuous delivery, integration, and deployment with AWS (Amazon Web Services).
+This project is an example of building microservices using FastAPI and integrating continuous delivery, integration, and deployment with AWS 
 
-## Technologies
-
-- [FastAPI](https://fastapi.tiangolo.com/): A modern, fast web framework for building APIs with Python.
-- AWS Services:
-  - [Amazon EC2](https://aws.amazon.com/ec2/): Virtual servers in the cloud.
-  - [Amazon RDS](https://aws.amazon.com/rds/): Managed relational database service.
-  - [Amazon S3](https://aws.amazon.com/s3/): Object storage service.
-- Continuous Integration/Continuous Deployment (CI/CD) tools (e.g., Jenkins, Travis CI, CircleCI) for automating deployments.
--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wikilogic
+from mylib.logic import phrase as wikiphrases
 
-print(wiki())
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    results = search_wiki(value)
+    return {"result": results}
+
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """Retrieve wikipedia page"""
+
+    results = wikilogic(name)
+    return {"result": results}
+
+
+@app.get("/phrase/{name}")
+async def phrase(name: str):
+    """Retrieve wikipedia page and return phrases"""
+    print(f"passed in name: {name}")
+    result = wikiphrases(name)
+    print(f"Result: {result}")
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
